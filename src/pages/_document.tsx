f0ced40b7fe05eedf7b16d51c5d1cc4602d070c0
Nikolay Lanets <n.lanets@freecode.academy>
2026-06-23 23:11:06 +0300

    Added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="icon" href="/modx.svg" type="image/x-icon" />
         </Head>
         <body>
           <Main />
